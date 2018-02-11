9c2f93f0c2b84a369f9a3bf4dac7c6d2e51e4eb0
Kentwh <kentwh@stud.ntnu.no>
2018-02-12 00:22:46 +0100

    tried drawing triangle, but not drawing

--- main.cpp
+++ main.cpp
@@ -4,8 +4,18 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+// Enumeration for buffer indexing, pluss numbers of avaliable buffers.
+enum {VB_POSITION, NUM_BUFFERS};
+
 // Open a window and create its OpenGL context
 GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
+
+// An array of 3 vectors which represents 3 vertices
+static const GLfloat vertecies[] = {
+    -1.0f, -1.0f,
+    1.0f, -1.0f,
+    0.0f,  1.0f,
+};
 
 int main()
 {
@@ -39,12 +49,51 @@ int main()
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+    // Our vertex array object (contains vbo's. MAX: 16)
+    GLuint vao;
+
+    // Out vertex buffer object (lives inside the vao)
+    GLuint vbo;
+
+    // Generate vao
+    glGenVertexArrays(1, &vao);
+
+    // Bind it for use
+    glBindVertexArray(vao);
+
+    // Gen vertex buffer object
+    glGenBuffers(1, &vbo);
+
+    // Bind buffer for use
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    // Fill the vbo with our vertecies
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertecies), vertecies, GL_STATIC_DRAW);
+
     // Draw loop
     do {
-        // Draw nothing, see you in tutorial 2 !
+        // Clear window with white color
+        //glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+        // Enable Vertex Attrib Array "VB_POSITION"
+        glEnableVertexAttribArray(VB_POSITION);
+
+        // Bind the vbo for drawing
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+        // Tell OpenGL aboutn the data and how it should read it
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        // Tell OpenGL to draw
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Disable the vbo "VB_POSITION"
+        glDisableVertexAttribArray(VB_POSITION);
 
         // Swap buffers
         glfwSwapBuffers(window);
+
+        // Poll event off event queue for handling
         glfwPollEvents();
 
     } // Check if the ESC key was pressed or the window was closed
